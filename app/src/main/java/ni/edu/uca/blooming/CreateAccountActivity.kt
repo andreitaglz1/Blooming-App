9b4c0810bbe501d8c908b9fbab852ce73403141a
andreitaglz1 <andrea.gonzalez14131@est.uca.edu.ni>
2022-11-13 14:45:55 -0600

    New Layouts

---
+++ app/src/main/java/ni/edu/uca/blooming/CreateAccountActivity.kt
@@ -0,0 +1,11 @@
+package ni.edu.uca.blooming
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateAccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+    }
+}
