44dd57a65baaae44b7ff8ec932146cfe725b399c
Blanca <blanca.rosales13620@est.uca.edu.ni>
2022-11-19 20:44:00 -0600

    Login codification implemented.

--- app/src/main/java/ni/edu/uca/blooming/CreateAccountActivity.kt
+++ app/src/main/java/ni/edu/uca/blooming/CreateAccountActivity.kt
@@ -2,10 +2,43 @@ package ni.edu.uca.blooming
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import ni.edu.uca.blooming.databinding.ActivityCreateAccountBinding
 
 class CreateAccountActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCreateAccountBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_account)
+        binding = ActivityCreateAccountBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        val auth = Firebase.auth
+        binding.btnSignUp.setOnClickListener {
+            val usuario: String = binding.etNombre.text.toString().trim()
+            val pwd: String = binding.etPassword.text.toString().trim()
+
+            auth.createUserWithEmailAndPassword(usuario, pwd).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Cuenta creada exitosamente", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Ha habido un error", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+
+
     }
+    
+
 }
--- app/src/main/java/ni/edu/uca/blooming/LoginActivity.kt
+++ app/src/main/java/ni/edu/uca/blooming/LoginActivity.kt
@@ -2,10 +2,35 @@ package ni.edu.uca.blooming
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import ni.edu.uca.blooming.databinding.ActivityCreateAccountBinding
+import ni.edu.uca.blooming.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        val auth = Firebase.auth
+        binding.btnLogin.setOnClickListener {
+            val usuario: String = binding.etNombre.text.toString().trim()
+            val pwd: String = binding.etPassword.text.toString().trim()
+
+            auth.signInWithEmailAndPassword(usuario, pwd).addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Has iniciado sesión", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Ha habido un error", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
+
